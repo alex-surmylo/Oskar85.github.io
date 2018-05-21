da8fbaaeded6b6911c3ff63f5f61a08cbfd06ea8
Alexandr <alexbal2010@gmail.com>
2018-05-21 11:47:53 +0300

    Add files via upload

---
+++ Ascend/js/style.js
@@ -0,0 +1,16 @@
+	$(document).ready(function() {
+	    $('.bxslider').bxSlider({
+	        auto: true,
+	        controls: false
+
+	    });
+
+
+	});
+
+	$(document).ready(function() {
+		$(".hamburger").click(function(){
+    	$(".menu").toggle(500);
+	});
+
+});
