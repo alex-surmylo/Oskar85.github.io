58fa07631ebf57e37e2cae072c2f15801440e25e
Oskar85 <alexbal2010@gmail.com>
2018-05-21 13:02:10 +0300

    Remove duplicated directory

--- Markupus_project/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    
-	$('.header-wrap__hamburger').on('click', function(){
-		$('.header-modal, .header__lang').toggleClass('active');
-			$('.footer').toggleClass('modal');
-	})
-
-});
