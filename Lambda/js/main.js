c5cfe1cca214799347495d15c2ec4c07f43b1342
Oskar85 <alexbal2010@gmail.com>
2019-03-01 16:07:02 +0200

    commit

---
+++ Lambda/js/main.js
@@ -0,0 +1,29 @@
+$( function() {
+    $( ".datepicker" ).datepicker({
+    		showAnim: "fadeIn",
+    		showOn: "both",
+    		buttonImageOnly: true,
+    		buttonImage: "../img/datapicker-icon.png"
+		})
+
+		$( ".selectmenu" ).selectmenu({
+			width: 200
+		});
+
+		$('.main-header__burger').on('click', function () {
+			$('.header-content__nav').toggleClass('is-open');
+			$('.main').toggleClass('is-open');
+			$('.main-content').toggleClass('is-open');
+		})
+
+
+		$(window).bind('resize', function() {
+			if ($(window).width() < 1020) {
+				$('.header-content__nav').height($('.main').height());
+			}	else {
+				$('.header-content__nav').height('auto');
+			}
+		});
+
+		
+});
